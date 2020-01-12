61e39142a97aeb658bbf25af98829c9972f0bf2c
EmelineGR <43139244+EmelineGR@users.noreply.github.com>
2020-01-12 13:27:45 +0100

    correction de condition de victoire et passage en ligue bronze !

--- Morpion/src/morpion.java
+++ Morpion/src/morpion.java
@@ -6,6 +6,7 @@ import java.math.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
 class Player {
     
     public static Case[][] Simulation(Case[][] grille, int x, int y, int val)
@@ -33,7 +34,7 @@ class Player {
     {
     
         if ( (partieFini(grilleAct) !=0) || profondeur == 0)
-            return partieFini(grilleAct)*(1+profondeur);
+            return partieFini(grilleAct);
         else {
             int score = 0;
             for (int i = 1; i <= profondeur; i++)
@@ -49,7 +50,6 @@ class Player {
     public static Case caseNumI(Case[][] grilleAct, int i)
     {
         int compteur = 1;
-        
         for (int k = 0; k < 3; k++)
         {
             for (int l = 0 ; l< 3; l++) 
@@ -78,35 +78,33 @@ class Player {
     {
         if( (grille[0][0].getVal() != 0) && (grille[0][0].getVal() == grille[1][1].getVal()) && (grille[1][1].getVal() == grille[2][2].getVal()) )
         {
-                return 10;
+                return 0;
         }
         if( (grille[1][1].getVal() != 0) && (grille[0][2].getVal() == grille[1][1].getVal()) && (grille[1][1].getVal() == grille[2][0].getVal()) )
         {
-
-                return 10;
+                return 0;
         }
 
         for(int i = 0; i < 3; i++)
         {
             if( (grille[i][0].getVal() != 0) && ( grille[i][0].getVal() == grille[i][1].getVal() ) && ( grille[i][1].getVal() == grille[i][2].getVal() ) )
             {
-                    return 10;
+                    return 0;
             }
         }
         for(int j = 0; j < 3; j++)
         {
             if( (grille[0][j].getVal() != 0 )  && ( grille[0][j].getVal() == grille[1][j].getVal() ) && ( grille[1][j].getVal() == grille[2][j].getVal() ) )
             {
-                    return 10;
+                    return 0;
             }
         }
-        return 0;
+        return 10;
     }
 
     public static void main(String args[]) 
     {
         Scanner in = new Scanner(System.in);
-
         Case[][] grille = new Case[3][3];
         for(int i=0; i<3; i++)
         {
@@ -170,7 +168,7 @@ class Player {
             }
             if(tourJeu >= 3)
             {
-                int max = -1000;
+                int min = 100000;
                 int caseAJoue = 1;
                 int score;
                 for(int g=1;g<(11-tourJeu);g++)
@@ -184,10 +182,9 @@ class Player {
                     System.err.println(score);
                     //System.err.println(min_max(Simulation(grille, X, Y, joueur), (6-tourJeu), joueur*(-1)));
                     
-                    
-                    if (score > max)
+                    if (score < min)
                     {
-                        max = score;
+                        min = score;
                         caseAJoue = g;
                     }
                 }
